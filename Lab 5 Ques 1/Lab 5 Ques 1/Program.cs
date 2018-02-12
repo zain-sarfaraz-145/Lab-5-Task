c90e8ebbf4269c035a2cf8375c18893cfea34829
zain-sarfaraz-145 <zainsarfaraz1996@gmail.com>
2018-02-12 20:36:50 +0500

    first commit

---
+++ Lab 5 Ques 1/Lab 5 Ques 1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lab_5_Ques_1
+{
+    public class information
+    {
+        public String naam, model_location;
+        public int weight_speed;
+            public information(string name, string location)
+            {
+                this.naam = name;
+                this.model_location = location;
+            }
+            public information(string driver, string model, int spd_wgh)
+            {
+                this.naam = driver;
+                this.model_location = model;
+                this.weight_speed = spd_wgh;
+            }
+
+        }
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                information cycle = new information("Rider", "NIPA"); 
+                information car = new information("Zain","R-4348",200);
+                information lorry = new information("Zain","T-7854",234);
+                Console.WriteLine("Cycle name is " + cycle.naam + " and location is " + cycle.model_location);
+                Console.WriteLine("Driver name is " + car.naam + " and model is " + car.model_location + " and speed is" + car.weight_speed+" km/h");
+                Console.WriteLine("Driver name is " + lorry.naam + " and model is " + lorry.model_location + "and weight is " + lorry.weight_speed+" KG");
+                Console.ReadKey();
+            }
+        }
+    }
---
+++ Lab 5 Ques 2/Lab 5 Ques 2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lab_5_Ques_2
+{
+    public struct Complex
+    {
+        public int real;
+        public int imaginary;
+
+        public Complex(int real, int imaginary)
+        {
+            this.real = real;
+            this.imaginary = imaginary;
+        }
+
+
+        public static Complex operator +(Complex c1, Complex c2)
+        {
+            return new Complex(c1.real + c2.real, c1.imaginary + c2.imaginary);
+        }
+
+
+        public override string ToString()
+        {
+            return (String.Format("{0} + {1}i", real, imaginary));
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Complex num1 = new Complex(2, 3);
+            Complex num2 = new Complex(3, 4);
+            Complex sum = num1 + num2;
+            Console.WriteLine("First Complex Number :  {0}", num1);
+            Console.WriteLine("Second Complex Number : {0}", num2);
+            Console.WriteLine("The Sum of the Two Numbers : {0}", sum);
+            Console.ReadLine();
+        }
+    }
+}
